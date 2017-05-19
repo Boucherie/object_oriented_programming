bc01d1a0dd7546765fb698daf662911aaabf52cc
Emily Boucher <emi.boucher@gmail.com>
2017-05-19 15:22:31 -0400

    exercise 1 Bank Account

---
+++ OOP_banking.rb
@@ -0,0 +1,21 @@
+class BankAccount
+
+attr_accessor :balance, :interest_rate
+
+  def initialize(balance, interest_rate)
+    @balance        = balance
+    @interest_rate  = interest_rate
+  end
+
+    def deposit(amount)
+      @balance += amount
+    end
+
+    def withdraw(amount)
+      @balance -= amount
+    end
+
+    def gain_interest
+      @balance = balance * (1 + interest_rate)
+    end
+end
